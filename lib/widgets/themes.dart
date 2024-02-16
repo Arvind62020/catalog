1978126eb907ef3abed3b8e9c9110c74b20b5258
Arvind62020 <arvind123.452@gmail.com>
2024-02-16 13:14:26 +0530

    day11 completed

---
+++ lib/widgets/themes.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+class MyTheme {
+  static ThemeData get themeData => ThemeData(
+        primarySwatch: Colors.deepPurple,
+      );
+}
