e4c33bc19ebcab3a66c57220b3597f5cd79655bf
Arvind62020 <arvind123.452@gmail.com>
2024-02-16 13:14:26 +0530

    day10 completed

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
+
+import '../widgets/drawer.dart';
+import '../widgets/item_widget.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "code pool";
+
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+        backgroundColor: Colors.cyan,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+            itemCount: dummyList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                item: dummyList[index],
+              );
+            }),
+      ),
+      drawer: MyDrawer(),
+    );
+  }
+}
